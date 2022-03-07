65ee55317bd9f34b246ea926c5cee66869e3ade0
22cath <cecollignon@gmail.com>
2022-03-07 23:49:14 +0100

    initial commit

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def la_funcion():
-    return "Hola, Flask!"
+    return "Hola, Mundo!"
 
 @app.route("/bye/<nombre>")
 def otra_funcion(nombre):
